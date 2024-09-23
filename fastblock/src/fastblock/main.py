24bc284a83304502a58b862ee94ddd2742ec8452
code-alexander <152750039+code-alexander@users.noreply.github.com>
2024-09-23 15:50:45 +1000

    Update main.py

--- fastblock/src/fastblock/main.py
+++ fastblock/src/fastblock/main.py
@@ -197,6 +197,7 @@ def get(id: str | None = None, uploaded: bool | None = None):
             id="code-input",
             name="code",
             placeholder="Paste your code here...",
+            style="min-height: 100px;",
         ),
         Button("Upload", id="upload-button", action="/", method="post", cls="contrast"),
         id="upload-section",
